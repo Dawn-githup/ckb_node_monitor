fe08fd5eb34d3e307b24193197b3d4bd83a7bc1f
Dawn-githup <13477199767@163.com>
2023-09-09 10:39:53 +0800

    first commit

---
+++ src/main/java/org/example/getUpdateTime.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import static io.restassured.RestAssured.given;
+
+public class getUpdateTime {
+//    public int getTime() {
+//        String time = given()
+//                .header("Content-Type","application/json; charset=utf-8")
+//                .get("https://api-nodes.ckb.dev/health").asString();
+//
+//        // 使用 Gson 解析 JSON 数组
+//        JsonArray jsonArrayObj = JsonParser.parseString(jsonArray).getAsJsonArray();
+//
+//        // 统计 JSON 对象的数量
+//        int objectCount = jsonArrayObj.size();
+//        System.out.println("JSON 数组中包含 " + objectCount + " 个 JSON 对象。");
+//        return objectCount;
+//    }
+}
---
+++ src/main/java/org/example/testCase.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import static io.restassured.RestAssured.given;
+
+public class testCase {
+    public static void main(String[] args) {
+        String time = given()
+                .header("Content-Type","application/json; charset=utf-8")
+                .get("https://api-nodes.ckb.dev/health").asString();
+        System.out.println(time);
+
+//        // 使用 Gson 解析 JSON 数组
+//        JsonArray jsonArrayObj = JsonParser.parseString(jsonArray).getAsJsonArray();
+//
+//        // 统计 JSON 对象的数量
+//        int objectCount = jsonArrayObj.size();
+//        System.out.println("JSON 数组中包含 " + objectCount + " 个 JSON 对象。");
+//        return objectCount;
+    }
+}
